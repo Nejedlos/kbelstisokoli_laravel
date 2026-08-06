aa2b1fc6ef426b1d7eac612a2a9d89735116d45f
Nejedlos <nejedlymi@gmail.com>
2026-08-06 08:07:00 +0200

    fix: remove unused team expected count calculation from TrainingController
    - Deleted the redundant `$teamExpectedCounts` logic and related query.
    - Simplified the `TrainingController` by removing unnecessary data fetching.

--- app/Http/Controllers/Public/TrainingController.php
+++ app/Http/Controllers/Public/TrainingController.php
@@ -12,19 +12,6 @@ class TrainingController extends Controller
     {
         $currentSeasonId = \App\Models\Season::where('is_active', true)->first()?->id;
 
-        // Načteme očekávané počty pro všechny týmy jednou, abychom je mohli sčítat u sdílených tréninků
-        $teamExpectedCounts = Team::withCount([
-            'activePlayers as expected_count' => function ($q) use ($currentSeasonId) {
-                if (! $currentSeasonId) {
-                    return $q->whereRaw('1 = 0');
-                }
-                $q->whereHas('user.userSeasonConfigs', function ($sq) use ($currentSeasonId) {
-                    $sq->where('season_id', $currentSeasonId)
-                        ->where('track_attendance', true);
-                });
-            },
-        ])->get()->pluck('expected_count', 'id');
-
         // Vynecháme virtuální tým "Celý klub" z hlavního výpisu
         $teams = Team::where('slug', '!=', 'klub')
             ->with(['trainings' => function ($query) {
